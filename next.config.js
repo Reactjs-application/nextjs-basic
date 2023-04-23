def0ec5092ea313c1ca333b2ca97893919266b36
deepakkurmi5 <deepakkurmi874@gmail.com>
2023-04-23 22:19:03 +0530

    configrations for nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,41 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = () => {
+  /** @type {import('next').NextConfig} */
 
-module.exports = nextConfig
+  const nextConfig = {
+    distDir: "build",
+    reactStrictMode: true,
+    env: {},
+    basePath: "",
+    swcMinify: true,
+    pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
+    publicRuntimeConfig: {
+      staticFolder: "/static",
+    },
+    webpack: (config, { isServer }) => {
+      const cfg = {
+        ...config,
+        resolve: {
+          ...config.resolve,
+        },
+      };
+
+      if (!isServer) {
+        cfg.optimization.splitChunks = {
+          chunks: "all",
+          minSize: 20480,
+          cacheGroups: {
+            default: {
+              reuseExistingChunk: true,
+              priority: -40,
+              minChunks: 2,
+            },
+          },
+        };
+      }
+
+      return cfg;
+    },
+  };
+
+  return nextConfig;
+};
